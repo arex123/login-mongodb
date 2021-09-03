c59d37e82930812ea9b9eca24f277c9c2947fb7f
arex123 <ad47kumar@gmail.com>
2021-09-03 18:06:44 +0530

    connected with db

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 // const mongoose = require("mongoose");
 var bodyParser=require("body-parser");
 
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/Tutorial',{
@@ -13,6 +16,38 @@ mongoose.connect('mongodb://localhost:27017/Tutorial',{
 }).then(()=>{
     console.log("DB Connected Successfully");
 });
+var db=mongoose.connection;
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+  
+var engine = require('consolidate');
+
+app.set('views', __dirname + '/views');
+app.engine('html', engine.mustache);
+app.set('view engine', 'html');
+
+app.post('/register', function(req,res){
+    var name = req.body.name;
+    var email =req.body.email;
+    var pass = req.body.password;
+  
+    var data = {
+        "name": name,
+        "email":email,
+        "password":pass,
+    }
+    db.collection('details').insertOne(data,function(err, collection){
+        if (err) throw err;
+        console.log("Record inserted Successfully");
+              
+    });
+          
+    return res.redirect('login.html');
+})
 
 // const db = require('./db');
 // // Connect to the database
@@ -26,42 +61,47 @@ require('dotenv').config()
 
 
 //user schema
-const userSchema = new mongoose.Schema({
-    username: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  })
- const User= mongoose.model("User", userSchema)
+// const userSchema = new mongoose.Schema({
+//     username: {
+//       type: String,
+//       required: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//     },
+//   })
+//  const User= mongoose.model("User", userSchema)
 
-  const stud = new User({
-      username: 'Madison Hyde',
-      email: 'xyz@email.com',
-      password : 333,
-});
-stud.save().then(() => console.log("One entry added"));
+//   const stud = new User({
+//       username: 'Madison Hyde',
+//       email: 'xyz@email.com',
+//       password : 333,
+// });
+// stud.save().then(() => console.log("One entry added"));
 
-const stud1 = new User({
-    username: 'Madison Hyde 2',
-    email: 'xycz@email.com',
-    password : 333,
-});
-stud1.save().then(() => console.log("One entry added"));
+// const stud1 = new User({
+//     username: 'Madison Hyde 2',
+//     email: 'xycz@email.com',
+//     password : 333,
+// });
+// stud1.save().then(() => console.log("One entry added"));
 
 
 
 
 
 app.get('/',(req,res) =>{
-    res.send('Hello World');
+    // res.send('Hello World');
+    // res.set({
+    //     'Access-control-Allow-Origin': '*'
+    //     });
+    // return 
+    res.render('index.html');
 });
 
 const PORT =process.env.PORT;
